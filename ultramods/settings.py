743f5e1dfe6e4d5754ce61b1ccc64469ee60d7e0
himanshp1656 <himanshp16@gmail.com>
2024-08-31 23:28:34 +0530

    Update settings.py

--- ultramods/settings.py
+++ ultramods/settings.py
@@ -75,24 +75,15 @@ WSGI_APPLICATION = 'ultramods.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'verceldb',
-        'USER': 'default',
-        'PASSWORD': 'kzxh1e4UpVnH',
-        'HOST': 'ep-weathered-smoke-a4itjsod-pooler.us-east-1.aws.neon.tech',
-        'PORT': '5432',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
